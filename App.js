89636fc18e1852b3fa75b271a201ea10ef265c79
manoharpatnaik <134580310+manoharpatnaik@users.noreply.github.com>
2023-09-23 14:07:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import DigitalClock from './DigitalClock';
+import Textcolor from './Textcolor';
+import Counter from './Counter';
+import Tabularform from './Tabularform';
+
+
+function App() {
+  return (
+    
+      
+<>
+<DigitalClock/>
+<Textcolor/>
+<Counter/>
+<Tabularform/>
+</>
+    
+  );
+}
+
+export default App;
